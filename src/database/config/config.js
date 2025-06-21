ea7219114851c0ea8c35359b884efea95d0b1875
JaviSoria19 <javisoar.2002@gmail.com>
2025-06-21 15:27:29 -0400

    Tercer hotfix consecutivo, se corrigió el archivo config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,6 +1,14 @@
-const { config } = require('dotenv');
-
 require('dotenv').config();
+
+const common = {
+  dialect: 'postgres',
+  dialectOptions: process.env.DB_SSL === 'true' ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  } : {}
+};
 
 module.exports = {
   development: {
@@ -9,12 +17,14 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres',
-    dialectOptions: process.env.DB_SSL === 'true' ? {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // This is important for self-signed certificates
-      }
-    } : {}
+    ...common
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    ...common
   }
 };
